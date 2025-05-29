c812121ad4771bbd163ec7e65859606cc5140715
Alex Knusel <alex.knusel@milwaukeetool.com>
2025-05-29 12:57:14 -0500

    Created npm project

---
+++ transformations/test-transformation.js
@@ -0,0 +1,8 @@
+/***
+ * This is a custom transformation template for building transformations from scratch.
+ * Learn more by visiting https://www.rudderstack.com/docs/features/transformations/templates/
+ ***/
+
+export function transformEvent(event, metadata) {
+  return event;
+}
